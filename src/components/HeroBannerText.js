46e5a52ed456fcb38a2532620fd07cc22dd12477
ruaraikirk <ruarai.kirk@optiim.com>
2020-03-21 14:45:02 +0000

    [FEATURE][0001] Initial version of hero landing section.

---
+++ src/components/HeroBannerText.js
@@ -0,0 +1,87 @@
+import React from "react"
+import {Grid, Icon} from '@material-ui/core';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import MailOutlineIcon from '@material-ui/icons/MailOutline';
+import InstagramIcon from '@material-ui/icons/Instagram';
+import styled, { keyframes }  from 'styled-components';
+
+const TypewriterTextLarge = keyframes`
+   from{width: 0;}
+   to{width: 420px;}
+`;
+
+const TypewriterTextSmall = keyframes`
+  from{width: 0;}
+  to{width: 310px;}
+`;
+
+const TypewriterCursor = keyframes`
+  from{border-right-color: rgba(0,255,0,.75);}
+  to{border-right-color: transparent;}
+`;
+
+const Typewriter = styled.div`
+  border-right: solid 3px rgba(0,255,0,.75);
+  white-space: nowrap;
+  overflow: hidden;
+  font-family: 'Source Code Pro', monospace;
+  color: #000000;
+  margin: 0 auto;
+  font-size: 28px;
+  @media screen and (min-width: 601px) {
+    font-size: 28px;
+    animation: ${TypewriterTextLarge} 3.5s steps(29,end) 1s 1 normal both,
+    ${TypewriterCursor} 600ms steps(29,end) infinite;
+  }
+  @media screen and (max-width: 600px) {
+    font-size: 21px;
+    animation: ${TypewriterTextSmall} 3s steps(29,end) 1s 1 normal both,
+    ${TypewriterCursor} 600ms steps(29,end) infinite;
+  }
+`;
+
+const StyledGrid = styled(Grid)`
+  flex-grow: 1;
+  width: 420px;
+`;
+
+const StyledIcon = styled(Icon)`
+  cursor: pointer
+`;
+
+const HeroBannerText = () => {
+  return (
+    <StyledGrid>
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <Typewriter>Hello World! I'm Ruaraí.</Typewriter>
+        </Grid>
+      </Grid>
+      <Grid container spacing={3}>
+        <Grid item xs={3}>
+          <StyledIcon>
+            <GitHubIcon fontSize="large" onClick={() => window.open('https://github.com/ruaraikirk')}/>
+          </StyledIcon>
+        </Grid>
+        <Grid item xs={3}>
+          <StyledIcon>
+            <LinkedInIcon fontSize="large" onClick={() => window.open('https://linkedin.com/in/ruaraikirk')}/>
+          </StyledIcon>
+        </Grid>
+        <Grid item xs={3}>
+          <StyledIcon>
+            <MailOutlineIcon fontSize="large" onClick={() => window.open('mailto: ruaraikirk@gmail.com')}/>
+          </StyledIcon>
+        </Grid>
+        <Grid item xs={3}>
+          <StyledIcon>
+            <InstagramIcon fontSize="large" onClick={() => window.open('https://www.instagram.com/ruaraikirk/?hl=en')}/>
+          </StyledIcon>
+        </Grid>
+      </Grid>
+    </StyledGrid>
+  )
+}
+
+export default HeroBannerText
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,42 +5,76 @@ import Bio from "../components/bio"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { rhythm } from "../utils/typography"
+import animateScrollTo from 'animated-scroll-to';
+import LazyHero from 'react-lazy-hero';
+import MouseIcon from "../components/MouseIcon"
+import HeroBannerText from "../components/HeroBannerText"
 
 const BlogIndex = ({ data, location }) => {
   const siteTitle = data.site.siteMetadata.title
   const posts = data.allMarkdownRemark.edges
+  const scrollWindow = () => {
+    const data = document.getElementById('to');
+    if (data) {
+      const windowScrollTop = window.scrollY || window.pageYOffset;
+      const dataRect = data.getBoundingClientRect();
+      animateScrollTo(dataRect.top + dataRect.height + windowScrollTop, {
+        speed: 1000,
+      });
+    }
+  };
 
   return (
-    <Layout location={location} title={siteTitle}>
-      <SEO title="All posts" />
-      <Bio />
-      {posts.map(({ node }) => {
-        const title = node.frontmatter.title || node.fields.slug
-        return (
-          <article key={node.fields.slug}>
-            <header>
-              <h3
-                style={{
-                  marginBottom: rhythm(1 / 4),
-                }}
-              >
-                <Link style={{ boxShadow: `none` }} to={node.fields.slug}>
-                  {title}
-                </Link>
-              </h3>
-              <small>{node.frontmatter.date}</small>
-            </header>
-            <section>
-              <p
-                dangerouslySetInnerHTML={{
-                  __html: node.frontmatter.description || node.excerpt,
-                }}
-              />
-            </section>
-          </article>
-        )
-      })}
-    </Layout>
+    <div>
+      <LazyHero
+        imageSrc="https://images.unsplash.com/photo-1457305237443-44c3d5a30b89?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2853&q=80"
+        minHeight="100vh"
+        parallaxOffset={100}
+        color="#D3D3D3"
+        transitionDuration={2500}
+      >
+        <HeroBannerText />
+        <span
+          role="button"
+          onClick={() => scrollWindow()}
+          onKeyDown={() => scrollWindow()}
+          tabIndex={0}
+        >
+        <MouseIcon onClick={scrollWindow} />
+      </span>
+      </LazyHero>
+      <div id="to" />
+      <Layout location={location} title={siteTitle}>
+        <SEO title="Portfolio" />
+        <Bio />
+        {posts.map(({ node }) => {
+          const title = node.frontmatter.title || node.fields.slug
+          return (
+            <article key={node.fields.slug}>
+              <header>
+                <h3
+                  style={{
+                    marginBottom: rhythm(1 / 4),
+                  }}
+                >
+                  <Link style={{ boxShadow: `none` }} to={node.fields.slug}>
+                    {title}
+                  </Link>
+                </h3>
+                <small>{node.frontmatter.date}</small>
+              </header>
+              <section>
+                <p
+                  dangerouslySetInnerHTML={{
+                    __html: node.frontmatter.description || node.excerpt,
+                  }}
+                />
+              </section>
+            </article>
+          )
+        })}
+      </Layout>
+    </div>
   )
 }
 
