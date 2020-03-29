a8fd86822f0b3fdf103fbeb4c117d4949cd54f52
ruaraikirk <ruarai.kirk@optiim.com>
2020-03-29 19:55:03 +0100

    FEATURE/0002/Initial commit using external JS, navigation issues.

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,5 +1,6 @@
 import Typography from "typography"
 import Wordpress2016 from "typography-theme-wordpress-2016"
+import deYoungTheme from 'typography-theme-de-young'
 
 Wordpress2016.overrideThemeStyles = () => {
   return {
@@ -9,9 +10,19 @@ Wordpress2016.overrideThemeStyles = () => {
   }
 }
 
+// deYoungTheme.overrideThemeStyles = () => {
+//   return {
+//     "a.gatsby-resp-image-link": {
+//       boxShadow: `none`,
+//     },
+//   }
+// }
+
 delete Wordpress2016.googleFonts
+// delete deYoungTheme.googleFonts
 
 const typography = new Typography(Wordpress2016)
+// const typography = new Typography(deYoungTheme)
 
 // Hot reload typography in development.
 if (process.env.NODE_ENV !== `production`) {
