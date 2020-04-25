232fd41483830fc2ca0f1799686fd4cc75cb2d85
ruaraikirk <ruarai.kirk@optiim.com>
2020-04-25 15:34:31 +0100

    [FEATURE][00004] Initial style appied to remaining sections.

--- src/sections/Opensource.js
+++ src/sections/Opensource.js
@@ -1,22 +1,42 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { graphql, Link, useStaticQuery } from "gatsby"
-import { Section } from "react-scroll-section"
+import Section from '../components/Section'
+import { Triangle } from "../components/Triangle"
+
+const { Container, Header } = Section;
+
+const Background = () => (
+  <div>
+    <Triangle
+      color="primaryDark"
+      height={['15vh', '30vh']}
+      width={['100vw', '100vw']}
+    />
+
+    <Triangle
+      color="secondary"
+      height={['50vh', '40vh']}
+      width={['70vw', '40vw']}
+      invertX
+      invertY
+    />
+
+    <Triangle
+      color="backgroundDark"
+      height={['40vh', '15vh']}
+      width={['100vw', '100vw']}
+      invertY
+    />
+  </div>
+);
 
 const Opensource = () => {
 
   return (
-    <Section id="opensource">
-      <div
-        style={{
-          height: '100vh',
-          width: '600px',
-          border: '5px solid #1C6EA4'
-        }}
-      >
-        Opensource
-      </div>
-    </Section>
+    <Container id="opensource" Background={Background}>
+      <Header name="Opensource" icon="🤝" label="handshake"  />
+    </Container>
   )
 }
 
