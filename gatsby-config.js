715724f03e2a4254028be03be02020b9fda3512d
ruaraikirk <ruarai.kirk@optiim.com>
2020-05-26 21:29:29 +0100

    [FEATURE][0010] Removed commented code.

--- gatsby-config.js
+++ gatsby-config.js
@@ -88,13 +88,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `GatsbyJS`,
+        name: `Ruaraí Kirk Portfolio`,
+        short_name: `RuaraíDev`,
         start_url: `/`,
         background_color: `#ffffff`,
-        theme_color: `#663399`,
+        theme_color: `#f2eaec`,
         display: `minimal-ui`,
-        icon: `content/assets/gatsby-icon.png`,
+        icon: `content/assets/favicon-code.png`,
       },
     },
     `gatsby-plugin-react-helmet`,
