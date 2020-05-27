350dbd0f106b3ce25336187b848e1fb9867a4c7f
ruaraikirk <ruarai.kirk@optiim.com>
2020-05-27 21:42:00 +0100

    [FEATURE][0011] Add comment.

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { ThemeProvider } from "./src/context/ThemeContext"
 
+// https://github.com/gatsbyjs/gatsby/issues/22552#issuecomment-603514399
 export const wrapRootElement = ({ element }) => (
   <ThemeProvider>{element}</ThemeProvider>
 )
